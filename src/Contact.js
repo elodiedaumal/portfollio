55cda02db7f68a7daadab212ffb31117a3580d0f
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-03-16 14:33:26 +0100

    created contact form

--- src/Contact.js
+++ src/Contact.js
@@ -20,47 +20,49 @@ const ContactForm = () => {
   }
 
   return (
-    <form
-      action={FORM_ENDPOINT}
-      onSubmit={handleSubmit}
-      method='POST'
-      target='_blank'
-    >
-      <div className='mb-3 pt-0'>
-        <input
-          type='text'
-          placeholder='Your name'
-          name='name'
-          className='px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full'
-          required
-        />
-      </div>
-      <div className='mb-3 pt-0'>
-        <input
-          type='email'
-          placeholder='Email'
-          name='email'
-          className='px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full'
-          required
-        />
-      </div>
-      <div className='mb-3 pt-0'>
-        <textarea
-          placeholder='Your message'
-          name='message'
-          className='px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full'
-          required
-        />
-      </div>
-      <div className='mb-3 pt-0'>
-        <button
-          className='bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-          type='submit'
-        >
-          Send a message
-        </button>
-      </div>
-    </form>
+    <section className='pt-1 md:flex md:px-10 flex-row-reverse max-w-screen-xl gap-20 mx-auto '>
+      <form
+        action={FORM_ENDPOINT}
+        onSubmit={handleSubmit}
+        method='POST'
+        target='_blank'
+      >
+        <div className='mb-3 pt-0'>
+          <input
+            type='text'
+            placeholder='Your name'
+            name='name'
+            className='px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full'
+            required
+          />
+        </div>
+        <div className='mb-3 pt-0'>
+          <input
+            type='email'
+            placeholder='Email'
+            name='email'
+            className='px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full'
+            required
+          />
+        </div>
+        <div className='mb-3 pt-0'>
+          <textarea
+            placeholder='Your message'
+            name='message'
+            className='px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full'
+            required
+          />
+        </div>
+        <div className='mb-3 pt-0'>
+          <button
+            className='bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+            type='submit'
+          >
+            Send a message
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
